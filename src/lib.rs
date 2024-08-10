ca8dd9e80f09740631f230bcea02bb904eaa23b2
Logan Grosz <logan.grosz@gmail.com>
2024-08-10 17:38:26 -0600

    Moves grade structure to hueco module

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,2 @@
-pub struct Grade {
-    value: u8,
-}
+pub mod hueco;
 
-impl Grade {
-    pub fn new(value: u8) -> Grade {
-        Grade { value }
-    }
-
-    pub fn value(& self) -> u8 {
-        self.value
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn new() {
-        assert_eq!(Grade::new(1).value(), 1);
-    }
-}
