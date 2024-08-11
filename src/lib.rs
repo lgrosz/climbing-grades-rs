1805bb75e4b423169bf9aad21b341a7d6b228e31
Logan Grosz <logan.grosz@gmail.com>
2024-08-11 14:01:48 -0600

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,31 @@
+/*
+ * TODO What makes a grade trait?
+ *
+ * - open-ended vs fixed
+ *
+ * Yosemite grade
+ * - fixed minimum
+ * - open top
+ * - refinement optional
+ * - ordering not well defined in some cases like 5.11 vs 5.11a
+ *
+ * Hueco grade
+ * - fixed minimum
+ * - open top
+ * - ordering always defined
+ *
+ * Fontainebleau grade
+ * - fixed minimum
+ * - open top
+ * - refinement required
+ * - ordering well defined
+ *
+ * B-grade
+ * - Fixed grades, only B1, B2, and B3
+ *
+ * Benesch Scale
+ * - Fixed levels, but counts down
+ * - Additional levels of 0 and 00
+ */
 pub mod hueco;
 
